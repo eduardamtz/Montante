2136b0dce6b03bbfcb5864a7deaf187e629f4cd5
Eduardo Aguilera <eduardoaguilera@Eduardos-MacBook-Air.local>
2013-04-14 21:30:13 -0500

    Initial Commit

---
+++ Montante/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Montante
+//
+//  Created by Eduardo Aguilera on 4/14/13.
+//  Copyright (c) 2013 Eduardo Aguilera. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
